b6c7ccad7f0750c08539aeddaa0197f6d32d533f
svrohith9 <svrohith9@gmail.com>
2021-09-14 21:06:32 -0400

    Simple Calculator

---
+++ SimpleCalculator/SimpleCalculator.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace SimpleCalculator
+{
+    partial class SimpleCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.opd1Txt = new System.Windows.Forms.TextBox();
+            this.optTxt = new System.Windows.Forms.TextBox();
+            this.opd2Txt = new System.Windows.Forms.TextBox();
+            this.resTxt = new System.Windows.Forms.TextBox();
+            this.opd1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // opd1Txt
+            // 
+            this.opd1Txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.opd1Txt.Location = new System.Drawing.Point(262, 89);
+            this.opd1Txt.Name = "opd1Txt";
+            this.opd1Txt.Size = new System.Drawing.Size(127, 31);
+            this.opd1Txt.TabIndex = 0;
+            this.opd1Txt.TextChanged += new System.EventHandler(this.opd1Txt_TextChanged);
+            // 
+            // optTxt
+            // 
+            this.optTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optTxt.Location = new System.Drawing.Point(262, 132);
+            this.optTxt.Name = "optTxt";
+            this.optTxt.Size = new System.Drawing.Size(38, 31);
+            this.optTxt.TabIndex = 1;
+            this.optTxt.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // opd2Txt
+            // 
+            this.opd2Txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.opd2Txt.Location = new System.Drawing.Point(262, 175);
+            this.opd2Txt.Name = "opd2Txt";
+            this.opd2Txt.Size = new System.Drawing.Size(100, 31);
+            this.opd2Txt.TabIndex = 2;
+            // 
+            // resTxt
+            // 
+            this.resTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resTxt.Location = new System.Drawing.Point(262, 214);
+            this.resTxt.Name = "resTxt";
+            this.resTxt.ReadOnly = true;
+            this.resTxt.Size = new System.Drawing.Size(100, 31);
+            this.resTxt.TabIndex = 3;
+            // 
+            // opd1
+            // 
+            this.opd1.AutoSize = true;
+            this.opd1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.opd1.Location = new System.Drawing.Point(92, 89);
+            this.opd1.Name = "opd1";
+            this.opd1.Size = new System.Drawing.Size(125, 25);
+            this.opd1.TabIndex = 4;
+            this.opd1.Text = "Operand 1: ";
+            this.opd1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(92, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Operator: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(92, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Operand 2: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(92, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Result: ";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(97, 312);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(120, 37);
+            this.btnCalculate.TabIndex = 8;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(262, 312);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(89, 37);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // SimpleCalculator
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(500, 385);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.opd1);
+            this.Controls.Add(this.resTxt);
+            this.Controls.Add(this.opd2Txt);
+            this.Controls.Add(this.optTxt);
+            this.Controls.Add(this.opd1Txt);
+            this.Name = "SimpleCalculator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simple Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox opd1Txt;
+        private System.Windows.Forms.TextBox optTxt;
+        private System.Windows.Forms.TextBox opd2Txt;
+        private System.Windows.Forms.TextBox resTxt;
+        private System.Windows.Forms.Label opd1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ SimpleCalculator/SimpleCalculator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculator
+{
+    public partial class SimpleCalculator : Form
+    {
+        public SimpleCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            decimal num1, num2;
+            string operand = optTxt.Text;
+            if ((operand == "+" || operand == "-" || operand == "/" || operand == "*")&&(Decimal.TryParse(opd1Txt.Text,out num1))&& (Decimal.TryParse(opd2Txt.Text, out num2)))
+            {
+                resTxt.Text = Calculate(num1, num2, operand).ToString();
+            }
+            else
+                MessageBox.Show("Cannot calculate due to invalid input!", "ERROR");
+            opd1Txt.Focus();
+        }
+
+        private decimal Calculate(decimal op1, decimal op2, String op)
+        {
+            decimal result;
+            if (op == "+")
+                result = op1 + op2;
+            else if (op == "-")
+                result = op1 - op2;
+            else if (op == "/")
+                result = op1 / op2;
+            else
+                result = op1 * op2;
+
+            return Math.Round(result,4);
+        }
+
+        private void opd1Txt_TextChanged(object sender, EventArgs e)
+        {
+            resTxt.Text = "";
+        }
+    }
+}
