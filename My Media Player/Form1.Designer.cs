8d4e02163df8f8e33611576b70e7a99a7b65442a
svrohith9 <svrohith9@gmail.com>
2021-11-13 13:37:25 -0500

    windows media player integration

---
+++ My Media Player/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace My_Media_Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnAddVideo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(44, 217);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 0;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(194, 217);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnAddVideo
+            // 
+            this.btnAddVideo.Location = new System.Drawing.Point(194, 362);
+            this.btnAddVideo.Name = "btnAddVideo";
+            this.btnAddVideo.Size = new System.Drawing.Size(75, 23);
+            this.btnAddVideo.TabIndex = 2;
+            this.btnAddVideo.Text = "Add";
+            this.btnAddVideo.UseVisualStyleBackColor = true;
+            this.btnAddVideo.Click += new System.EventHandler(this.btnAddVideo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Select a video to play:  ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 258);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(228, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter filename of a new video to add to the list:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(44, 324);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(42, 17);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = ".avi";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(111, 324);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(48, 17);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = ".mp3";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(185, 324);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(50, 17);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = ".wmv";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(44, 285);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(225, 20);
+            this.tbPath.TabIndex = 8;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(44, 66);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(225, 134);
+            this.listBox1.Sorted = true;
+            this.listBox1.TabIndex = 9;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(293, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(563, 373);
+            this.axWindowsMediaPlayer1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 398);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAddVideo);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnRemove);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "My Media Player";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnAddVideo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.ListBox listBox1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+    }
+}
+
---
+++ My Media Player/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_Media_Player
+{
+    public partial class Form1 : Form
+    {
+        string path = @"C:\Media\";
+        Dictionary<string, string> mediaFiles = new Dictionary<string, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddVideo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (tbPath.Text.ToString() != "")
+                    path += tbPath.Text.ToString();
+                else
+                    throw new Exception("No file name is entered");
+                if (radioButton1.Checked == true && radioButton2.Checked == false && radioButton3.Checked == false)
+                    path += ".avi";
+                else if (radioButton1.Checked == false && radioButton2.Checked == true && radioButton3.Checked == false)
+                    path += ".mp3";
+                else if (radioButton1.Checked == false && radioButton2.Checked == false && radioButton3.Checked == true)
+                    path += ".wmv";
+                else
+                    throw new Exception("None of the three radio buttons is selected.");
+
+                if (mediaFiles.ContainsKey(tbPath.Text.ToString()))
+                    throw new Exception("Duplicate value entered");
+
+                mediaFiles.Add(tbPath.Text.ToString(), path);
+                this.listBox1.Items.Add(tbPath.Text.ToString());
+                tbPath.Text = "";
+                radioButton1.Checked = false;
+                radioButton2.Checked = false;
+                radioButton3.Checked = false;
+                path = @"C:\Media\";
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message.ToString(), "Error");
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            this.axWindowsMediaPlayer1.URL = "";
+            try
+            {
+                if (listBox1.SelectedIndex == -1)
+                    throw new Exception("No file is selected");
+                else
+                {
+                    Console.WriteLine(listBox1.SelectedItem);
+                    this.mediaFiles.Remove(listBox1.SelectedItem.ToString());
+                    this.listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message.ToString(), "Error");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.listBox1.Items.Add("Cartoon");
+            this.listBox1.Items.Add("Wildlife");
+
+            this.mediaFiles.Add("Cartoon", @"C:\Media\Cartoon.avi");
+            this.mediaFiles.Add("Wildlife", @"C:\Media\Wildlife.wmv");
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listBox1.SelectedIndex == -1)
+                    throw new Exception("No file is selected");
+                else
+                {
+                    if (this.mediaFiles.TryGetValue(this.listBox1.SelectedItem.ToString(), out string url))
+                        axWindowsMediaPlayer1.URL = url;
+                    else
+                        throw new Exception("No File Found");
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message.ToString(), "Error");
+            }
+
+        }
+    }
+}
